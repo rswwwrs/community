0a416fb9608d4cdc6985b82dfe84ff8dc602a9f7
QvQ <841377043@qq.com>
2019-11-28 21:23:17 +0800

    login auth by git and extract config from code to properties

---
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -0,0 +1,43 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import life.majiang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.edirect.uri}")
+    private String redirectUri;
+
+
+    //登录按钮访问github的authorize,回调callback函数,getAccessToken()获取 access_token ;getUser()通过 获取access_token 获取用户信息
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        //登录成功后返回主页
+        return "index";
+    }
+}
---
+++ src/main/java/life/majiang/community/provider/GithubProvider.java
@@ -0,0 +1,50 @@
+package life.majiang.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GithubProvider {
+    //2.OKHTTP发送POST请求封装AccessTokenDTO访问https://github.com/login/oauth/access_token, 获取access_token
+    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String str = response.body().string();
+            //System.out.println(str);
+            //access_token=032719e280ab61a453cb3c38ae1a906368451fd3&scope=user&token_type=bearer
+            String access_token = str.split("&")[0].split("=")[1];
+            return access_token;
+        } catch (Exception e) {
+        }
+        return null;
+
+    }
+    //3.OKHTTP发送GET请求,通过AccessToken获取用户信息
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+accessToken)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            String str = response.body().string();
+            GithubUser githubUser = JSON.parseObject(str, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+        }
+        return null;
+    }
+
+}
